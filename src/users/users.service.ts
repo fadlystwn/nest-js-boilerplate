1614bcb002f581830cbfedbbb0f1dc827372fde3
fadlydev <fadly.s@wistkey.dev>
2024-11-07 15:34:25 +0700

    add Error handling

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,40 +1,69 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { User, UserDocument } from './users.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateUserDto } from './dto/create-users.dto';
 import { UpdateUserDto } from './dto/update-users.dto';
+
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async create(createUserDto: CreateUserDto): Promise<UserDocument> {
-    return this.userModel.create(createUserDto);
+    try {
+      const createdUser = await this.userModel.create(createUserDto);
+      return createdUser;
+    } catch (error) {
+      throw new BadRequestException('Error creating user: ' + error.message);
+    }
   }
 
   async findAll(): Promise<UserDocument[]> {
-    return this.userModel.find().exec();
+    try {
+      return await this.userModel.find().exec();
+    } catch (error) {
+      throw new BadRequestException('Error fetching users: ' + error.message);
+    }
   }
 
   async update(
     id: string,
     updateUserDto: UpdateUserDto,
   ): Promise<UserDocument> {
-    const updateUser = this.userModel.findOneAndUpdate(
-      { _id: id },
-      updateUserDto,
-      {
-        new: true,
-      },
-    );
+    try {
+      const updatedUser = await this.userModel.findOneAndUpdate(
+        { _id: id },
+        updateUserDto,
+        { new: true },
+      );
 
-    if (!updateUser) {
-      throw new NotFoundException(`User with id ${id} not found`);
+      if (!updatedUser) {
+        throw new NotFoundException(`User with id ${id} not found`);
+      }
+      return updatedUser;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new BadRequestException('Error updating user: ' + error.message);
     }
-    return updateUser;
   }
 
   async delete(id: string): Promise<UserDocument | null> {
-    return this.userModel.findByIdAndDelete({ _id: id });
+    try {
+      const deletedUser = await this.userModel.findByIdAndDelete(id);
+
+      if (!deletedUser) {
+        throw new NotFoundException(`User with id ${id} not found`);
+      }
+
+      return deletedUser;
+    } catch (error) {
+      throw new BadRequestException('Error deleting user: ' + error.message);
+    }
   }
 }
