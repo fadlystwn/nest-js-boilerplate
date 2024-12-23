8f81e7ee19f3cbe1c27a2f67e4e01d7edacaa286
fadlydev <fadlystwn@gmail.com>
2024-12-24 06:00:23 +0700

    add username endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,7 @@ import {
   Controller,
   Get,
   Delete,
+  NotFoundException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-users.dto';
@@ -24,6 +25,15 @@ export class UsersController {
   @Get()
   async findAll(): Promise<UserDocument[]> {
     return this.usersService.findAll();
+  }
+
+  @Get(':username')
+  async findOne(@Param('username') username: string): Promise<UserDocument> {
+    const user = await this.usersService.findOne(username);
+    if (!user) {
+      throw new NotFoundException(`User with username ${username} not found`);
+    }
+    return user;
   }
 
   @Patch(':id')
